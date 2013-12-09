97e62095afe43dfeb6b7879c81b42852d8c3e11f
Ivan Marcin <ivan.marcin@mylookout.com>
2013-12-09 01:40:52 -0800

    Card printing and nice formatting

--- lib/blackjack/cards/card_types.rb
+++ lib/blackjack/cards/card_types.rb
@@ -1,19 +1,21 @@
 # encoding: UTF-8
+require 'colorize'
+
 class FrenchDeck
   TYPE = {
-    1  => :ace,
-    2  => :two,
-    3  => :three,
-    4  => :four,
-    5  => :five,
-    6  => :six,
-    7  => :seven,
-    8  => :eight,
-    9  => :nine,
-    10 => :ten,
-    11 => :jack,
-    12 => :queen,
-    13 => :king
+    :ace    => 1,
+    :two    => 2,
+    :three  => 3,
+    :four   => 4,
+    :five   => 5,
+    :six    => 6,
+    :seven  => 7,
+    :eight  => 8,
+    :nine   => 9,
+    :ten    => 10,
+    :jack   => 11,
+    :queen  => 12,
+    :king   => 13
   }.freeze
 
   SUIT = {
@@ -22,6 +24,22 @@ class FrenchDeck
     :diamond => "♦",
     :clove   => "♣"
   }.freeze
+
+  def self.card_face(card_type)
+    case TYPE[card_type]
+    when 2..10
+      TYPE[card_type]
+    when 1
+      "A"
+    when 11
+      "J"
+    when 12
+      "Q"
+    when 13
+      "K"
+    end
+  end
+
 end
 
 class Card
@@ -32,4 +50,19 @@ class Card
     @suit = card_suit
     @type = card_type
   end
+
+  def to_s
+    return "#{FrenchDeck.card_face(@type)}#{FrenchDeck::SUIT[@suit]}"
+  end
+
+  def print
+    case @suit
+    when :heart, :diamond
+      text = "[#{to_s}]".red.on_white
+    else
+      text = "[#{to_s}]".black.on_white
+    end
+    puts text
+  end
+
 end
--- spec/unit/cards/deck_spec.rb
+++ spec/unit/cards/deck_spec.rb
@@ -8,13 +8,23 @@ module Cards
       end
     end
 
-    describe '#shuffle'  do
+    describe '#shuffle' do
       it 'shuffled' do
         bj_deck = Deck.build_deck
         shuffled_deck = Deck.shuffle(bj_deck)
 
         expect(bj_deck).not_to eq(shuffled_deck)
         expect(bj_deck).to eq(bj_deck & shuffled_deck)
+      end
+    end
+
+    describe '#print' do
+      it 'prints nicely' do
+        expect(Card.new(:spade,:king).to_s).to eq "K♠"
+        expect(Card.new(:heart,:jack).to_s).to eq "J♥"
+        expect(Card.new(:diamond,:queen).to_s).to eq "Q♦"
+        expect(Card.new(:clove,:ace).to_s).to eq "A♣"
+        expect(Card.new(:clove,:three).to_s).to eq "3♣"
       end
     end
   end
